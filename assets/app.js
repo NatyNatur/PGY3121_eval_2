e4e4f228e0b6c4ff3f3da1471eb876225d153274
Natalia <naticcg@gmail.com>
2022-06-02 19:05:36 -0400

    validaciones y modal con datos ingresados

--- assets/app.js
+++ assets/app.js
@@ -1,77 +1,127 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
+$(document).ready(function () {
+    $(window).scroll(function () {
         var scroll = $(window).scrollTop();
         if (scroll > 20) {
-          $(".navChange").css("background" , "#182D65");
+            $(".navChange").css("background", "#182D65");
         }
-  
-        else{
-            $(".navChange").css("background" , "transparent");  	
+        else {
+            $(".navChange").css("background", "transparent");
         }
     })
-  });
+});
 
-  function enviaMensaje() {
+function enviaMensaje() {
     var rut = document.formulario.rut.value;
     var nombre = document.formulario.nombre.value;
     var edad = document.formulario.edad.value;
     var genero = document.formulario.genero.value;
     var email = document.formulario.email.value;
-    var telefono = document.formulario.telefono;
-    var region = document.formulario.region;
-    var mensaje = document.formulario.mensaje;
-    var contDatos = document.getElementById('datos');
+    var telefono = document.formulario.telefono.value;
+    var region = document.formulario.region.value;
+    var mensaje = document.formulario.mensaje.value;
+    var contDatos = document.getElementById('listaDatos');
+    $('#listaDatos').empty();
 
-
-    if (rut.length < 9 || rut.length > 10) {
-        alert('Debe ingresar un rut válido.')
-        document.formulario.rut.focus();
-        return false;
-    } 
-    else {
-        var rutNode = document.createElement("p");
-        var rutTextNode = document.createTextNode("Rut ingresado es: " + rut);
-        rutNode.appendChild(rutTextNode);
-    }
-
-    if (nombre.length < 2) {
+    if (nombre.length < 3) {
         alert('Debe ingresar un nombre válido');
         document.formulario.nombre.focus();
         return false;
-    } 
+    }
     else {
-        var nameNode = document.createElement("p");
-        var nameTextNode = document.createTextNode("Nombre ingresado es: " + nombre);
+        var nameNode = document.createElement("li");
+        var nameTextNode = document.createTextNode("Nombre: " + nombre);
         nameNode.appendChild(nameTextNode);
     }
 
+    if (rut.length < 8 || rut.length > 11 || rut.indexOf('-') === -1) {
+        alert('Debe ingresar un rut válido con guión.')
+        document.formulario.rut.focus();
+        return false;
+    }
+    else {
+        var rutNode = document.createElement("li");
+        var rutTextNode = document.createTextNode("Rut: " + rut);
+        rutNode.appendChild(rutTextNode);
+    }
+
     if (edad <= 0 || edad === '') {
-        alert('Debe ser mayor a 18 años y menor de 65 años.');
+        alert('Debe ingresar su edad.');
         document.formulario.edad.focus();
         return false;
-    } 
+    }
     else {
-        var edadNode = document.createElement("p");
-        var edadTextNode = document.createTextNode("Edad ingresado es: " + edad);
+        var edadNode = document.createElement("li");
+        var edadTextNode = document.createTextNode("Edad: " + edad);
         edadNode.appendChild(edadTextNode);
     }
 
-    if (email.indexOf('@') === -1 || email.indexOf('.') === -1) {
+    if (genero === '') {
+        alert('Debe seleccionar un género.');
+        document.formulario.genero.focus();
+        return false;
+    } else {
+        var generoNode = document.createElement("li");
+        var generoTextNode = document.createTextNode("Género: " + genero);
+        generoNode.appendChild(generoTextNode);
+    }
+
+    if (email.indexOf('@') === -1 || email.indexOf('.') === -1 || email.length < 3) {
         document.formulario.email.focus();
-        alert('Debe ingresar un email válido.')
-    } 
+        alert('Debe ingresar un email válido.');
+        return false;
+    }
     else {
-        var emailNode = document.createElement("p");
-        var emailTextNode = document.createTextNode("Correo ingresado es: " + email);
+        var emailNode = document.createElement("li");
+        var emailTextNode = document.createTextNode("Correo: " + email);
         emailNode.appendChild(emailTextNode);
     }
 
-    contDatos.appendChild(rutNode);
+    if (telefono.length != 9 || telefono.indexOf('+') === 1 || telefono[0] != '9') {
+        alert('Debe ingresar un teléfono válido sin +. Recuerde que debe comenzar con 9.')
+        document.formulario.telefono.focus();
+        return false;
+    }
+    else {
+        var telefonoNode = document.createElement("li");
+        var telefonoTextNode = document.createTextNode("Teléfono: " + telefono);
+        telefonoNode.appendChild(telefonoTextNode);
+    }
+
+    if (region === 'null') {
+        alert('Debe seleccionar una región.');
+        document.formulario.region.focus();
+        return false;
+    } else {
+        var regionNode = document.createElement("li");
+        var regionTextNode = document.createTextNode("Región: " + region);
+        regionNode.appendChild(regionTextNode);
+    }
+
+    if (mensaje.length < 3) {
+        alert('Recuerde dejar su mensaje.');
+        document.formulario.mensaje.focus();
+        return false;
+    }
+    else {
+        var mensajeNode = document.createElement("li");
+        var mensajeTextNode = document.createTextNode("Mensaje: " + mensaje);
+        mensajeNode.appendChild(mensajeTextNode);
+    }
+
     contDatos.appendChild(nameNode);
+    contDatos.appendChild(rutNode);
     contDatos.appendChild(edadNode);
+    contDatos.appendChild(generoNode);
     contDatos.appendChild(emailNode);
+    contDatos.appendChild(telefonoNode);
+    contDatos.appendChild(mensajeNode);
+    $('#modalDatos').modal('show')
 }
 
+$('#modalDatos').on('hidden.bs.modal', function (event) {
+    limpiarForm();
+})
+
 function limpiarForm() {
-  document.formulario.reset();
+    document.formulario.reset();
 }
